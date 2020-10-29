40a49fd402d97b26d730076dab08c545e89a0f63
Sean Stacey <37584296+seantstacey@users.noreply.github.com>
2020-10-29 17:14:32 -0400

    Update myConnectInfo.py
    This file has been updated to include the updates that were made for the third post in the blog series.  The main additions is for os.environ

--- myConnectInfo.py
+++ myConnectInfo.py
@@ -8,4 +8,15 @@ dsn= """(DESCRIPTION =
      (CONNECT_DATA =
      (SERVER = DEDICATED)
      (SERVICE_NAME = pdb1.localdomain)))"""
+#
+# This file is updated in the third blog post.  The contents below are for that post - Connecting to the Autonomous JSON Database:
+#
+import os
+os.environ['TNS_ADMIN'] = '/home/oracle/Python_Demo/Wallet_AJDBSEANS'
 
+# Username
+usrnm="scott"
+# Password
+psswd="<my password>"
+# Data Source Name
+dsn= "ajdbseans_tp"
